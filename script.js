204927cc06054f4e772c014ee8e18aaff92c0157
SStrange1024-000 <136175757+SStrange1024-000@users.noreply.github.com>
2025-09-04 02:11:34 +0530

    adding all files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const NavBar=document.querySelector('.nav-links');
+const Navbtn=document.querySelector('.home-btn');
+
+Navbtn.addEventListener('click',()=>{
+    NavBar.classList.toggle('mobile-nav')
+
+})
+NavBar.addEventListener('click',()=>{
+    NavBar.classList.toggle('mobile-nav')
+
+})
